8b2b9ac5cacced39ab4b990c052b60072ee8e374
Nieves <niieves99@gmail.com>
2023-07-20 14:30:32 +0200

    de vuelta a datos a consulta okey

--- my_cripto/consulta.py
+++ my_cripto/consulta.py
@@ -3,20 +3,36 @@ import requests
 apikey = "1B731114-B858-433E-89F1-A792584BE95B"
 
 
+
+
+
+"""
+tenemos json 
+sacar variables json ---- from_moneda /from_cantidad/ to_moneda x
+formatear la api con las monedas x
+cogemos el response (que es el precio unitario)x
+-metemos el preciop unitario en una valiable para devolver x
+- calculamos to_cantidad = response*from_cantidad x
+-capturar errores
+-lo empaquetamos muy bonito y lo mandamos a js
+"""
+
+
+
 def get_to_cantidad(data):
     from_moneda = data["from_moneda"]
     to_moneda = data["to_moneda"]
-    #from_cantidad = data["from_cantidad"]
+    from_cantidad = data["from_cantidad"]
     url=f"https://rest.coinapi.io/v1/exchangerate/{from_moneda}/{to_moneda}?apikey={apikey}"
     try:
         response = requests.get(url)
         respuesta = response.json()
         precio_unitario =  respuesta["rate"]
-        #to_cantidad = precio_unitario * float(from_cantidad)
+        to_cantidad = precio_unitario * float(from_cantidad)
         if response.status_code == 200:
-            return precio_unitario
+            return to_cantidad
     except:
-        return response.status_code,"-",response["error"] #esto no va
+        return 5
 
 
 
